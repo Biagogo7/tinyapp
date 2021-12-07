58dc6f60fddfafb346a9bfb588c6d3e9de43c391
Adeyemi <henry.atayero@gmail.com>
2021-12-06 21:10:34 -0600

    updating express_server.js

--- express_server.js
+++ express_server.js
@@ -22,6 +22,11 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+app.get("/urls/:shortURL", (req, res) => {
+  const templateVars = { shortURL: req.params.shortURL, longURL: "http://www.lighthouselabs.ca" };
+  res.render("urls_show", templateVars);
+});
+
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
